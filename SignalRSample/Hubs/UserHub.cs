3dfded002dd0357027f92db1402d4495011eda15
Rana Hamid <rhamid@mymedicalhub.com>
2024-09-09 13:10:39 +0600

    Add project files.

---
+++ SignalRSample/Hubs/UserHub.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace SignalRSample.Hubs
+{
+    public class UserHub:Hub
+    {
+        public static int TotalViews { get; set; } = 0;
+        public async Task NewWindowLoaded()
+        {
+            TotalViews++;
+            await Clients.All.SendAsync("updateTotalViews", TotalViews);
+        }
+    }
+}
